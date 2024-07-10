ee7c24216d0cff8d8865f03b03c53910e2da5491
alfianadis <alfianadiseptianto@gmail.com>
2024-07-10 20:22:04 +0700

    login n regis

---
+++ src/auth/dto/update-user.dto.ts
@@ -0,0 +1,19 @@
+import { IsString, IsOptional } from 'class-validator';
+
+export class UpdateUserDto {
+  @IsString()
+  @IsOptional()
+  readonly username?: string;
+
+  @IsString()
+  @IsOptional()
+  readonly password?: string;
+
+  @IsString()
+  @IsOptional()
+  readonly fullName?: string;
+
+  @IsString()
+  @IsOptional()
+  readonly role?: string;
+}
