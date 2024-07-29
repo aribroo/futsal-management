dcc664899236a34b3482ff6deddd1e23fa840c44
aribroo <rifkiari123@gmail.com>
2024-07-29 11:27:56 +0700

    fix

--- src/emotional/create-emotional.dto.ts
+++ src/emotional/create-emotional.dto.ts
@@ -2,39 +2,39 @@ import { IsString } from 'class-validator';
 import { ApiProperty } from '@nestjs/swagger';
 
 export class CreateEmotionalDto {
-  @ApiProperty({ descriptions: 'Posisi', example: 'Anchor' })
+  @ApiProperty({ description: 'Posisi', example: 'Anchor' })
   @IsString() 
   position: string;
 
-  @ApiProperty({ descriptions: 'Nama Pemain', example: 'Alfian Adi' })
+  @ApiProperty({ description: 'Nama Pemain', example: 'Alfian Adi' })
   @IsString() 
   name: string;
 
-  @ApiProperty({ descriptions: 'Minggu Penilaian', example: 'Week 1' })
+  @ApiProperty({ description: 'Minggu Penilaian', example: 'Week 1' })
   @IsString() 
   assessmentWeek: string;
 
-  @ApiProperty({ descriptions: 'Nilai Disiplin', example: 'Baik' })
+  @ApiProperty({ description: 'Nilai Disiplin', example: 'Baik' })
   @IsString() 
   disciplineScore: string;
 
-  @ApiProperty({ descriptions: 'Nilai Motivasi', example: 'Baik' })
+  @ApiProperty({ description: 'Nilai Motivasi', example: 'Baik' })
   @IsString() 
   motivationScore: string;
 
-  @ApiProperty({ descriptions: 'Nilai', example: 'Baik' })
+  @ApiProperty({ description: 'Nilai', example: 'Baik' })
   @IsString() 
   leadershipScore: string;
 
-  @ApiProperty({ descriptions: 'Nilai', example: 'Baik' })
+  @ApiProperty({ description: 'Nilai', example: 'Baik' })
   @IsString() 
   teamworkScore: string;
 
-  @ApiProperty({ descriptions: 'Nilai', example: 'Baik' })
+  @ApiProperty({ description: 'Nilai', example: 'Baik' })
   @IsString() 
   emotionalControlScore: string;
 
-  @ApiProperty({ descriptions: 'Nilai', example: 'Baik' })
+  @ApiProperty({ description: 'Nilai', example: 'Baik' })
   @IsString() 
   developmentScore: string;
 }
